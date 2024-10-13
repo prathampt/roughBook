4e745a4cbb89e48ed150f807a63868e7c26308d9
Pratham Tandale <prathamt12345@gmail.com>
2024-10-13 20:31:03 +0530

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require("cors");
 const pageRoutes = require('./routes/pages')
 const quoteRoutes = require('./routes/quotes')
 
 const app = express()
 
 // middleware
+app.use(cors());
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -20,10 +22,10 @@ app.use('/api/quotes', quoteRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
+    console.log('Connected to database')
     // listen to port
     app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+      console.log('Listening for requests on port', process.env.PORT)
     })
   })
   .catch((err) => {
